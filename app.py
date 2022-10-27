e6f943b0e404057c3662f3c40005ab1aa9715171
eden <eden.lecarpentier.simplon@gmail.com>
2022-10-27 10:34:44 +0200

    rapport

--- app.py
+++ app.py
@@ -7,31 +7,31 @@ model = pickle.load(open('pickle_model/model_pkl.pickle', 'rb'))
 
 def run():
     
-    st.title("Bank Loan Prediction using Machine Learning")
+    st.title("Prédiction de prêt bancaire")
 
     ## Account No
-    account_no = st.text_input('Account number')
+    account_no = st.text_input('Numéro du compte')
 
     ## Full Name
-    fn = st.text_input('Full Name')
+    fn = st.text_input('Nom / Prénom')
 
     ## For gender
-    gen_display = ('Female','Male')
+    gen_display = ('Femme','Homme')
     gen_options = list(range(len(gen_display)))
     gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
 
     ## For Marital Status
-    mar_display = ('No','Yes')
+    mar_display = ('Non','Oui')
     mar_options = list(range(len(mar_display)))
-    mar = st.selectbox("Marital Status", mar_options, format_func=lambda x: mar_display[x])
+    mar = st.selectbox("Etat civil", mar_options, format_func=lambda x: mar_display[x])
 
     ## No of dependets
-    dep_display = ('No','One','Two','More than Two')
+    dep_display = ('Non','Un','Deux','Plus de deux')
     dep_options = list(range(len(dep_display)))
-    dep = st.selectbox("Dependents",  dep_options, format_func=lambda x: dep_display[x])
+    dep = st.selectbox("Dépendents",  dep_options, format_func=lambda x: dep_display[x])
 
     ## For edu
-    edu_display = ('Not Graduate','Graduate')
+    edu_display = ('Non diplômé','Diplômé')
     edu_options = list(range(len(edu_display)))
     edu = st.selectbox("Education",edu_options, format_func=lambda x: edu_display[x])
 
@@ -43,28 +43,28 @@ def run():
     ## For Property status
     prop_display = ('Rural','Semi-Urban','Urban')
     prop_options = list(range(len(prop_display)))
-    prop = st.selectbox("Property Area",prop_options, format_func=lambda x: prop_display[x])
+    prop = st.selectbox("Endroit de résidence",prop_options, format_func=lambda x: prop_display[x])
 
     ## For Credit Score
-    cred_display = ('Between 300 to 500','Above 500')
+    cred_display = ('0','1')
     cred_options = list(range(len(cred_display)))
-    cred = st.selectbox("Credit Score",cred_options, format_func=lambda x: cred_display[x])
+    cred = st.selectbox("Credit history",cred_options, format_func=lambda x: cred_display[x])
 
     ## Applicant Monthly Income
-    mon_income = st.number_input("Applicant's Monthly Income($)",value=0)
+    mon_income = st.number_input("Salaire mensuelle du demandeur",value=0)
 
     ## Co-Applicant Monthly Income
-    co_mon_income = st.number_input("Co-Applicant's Monthly Income($)",value=0)
+    co_mon_income = st.number_input("Salaire mensuelle du codemandeur)",value=0)
 
     ## Loan AMount
-    loan_amt = st.number_input("Loan Amount",value=0)
+    loan_amt = st.number_input("Prix du prêt",value=0)
 
     ## loan duration
-    dur_display = ['2 Month','6 Month','8 Month','1 Year','16 Month']
+    dur_display = ['2 Mois','6 Mois','8 Mois','1 Année','16 Mois']
     dur_options = range(len(dur_display))
-    dur = st.selectbox("Loan Duration",dur_options, format_func=lambda x: dur_display[x])
+    dur = st.selectbox("Durée du prêt",dur_options, format_func=lambda x: dur_display[x])
 
-    if st.button("Submit"):
+    if st.button("Prédiction du prêt"):
         duration = 0
         if dur == 0:
             duration = 60
@@ -83,15 +83,15 @@ def run():
         ans = int("".join(lc))
         if ans == 0:
             st.error(
-                "Hello: " + fn +" || "
-                "Account number: "+account_no +' || '
-                'According to our Calculations, you will not get the loan from Bank'
+                "Bonjour: " + fn +" || "
+                "Numéro de compte: "+account_no +' || '
+                'Suite à nos calcul vous ne pouvez pas prétendre à un prêt bancaire.'
             )
         else:
             st.success(
-                "Hello: " + fn +" || "
-                "Account number: "+account_no +' || '
-                'Congratulations!! you will get the loan from Bank'
+                "Bonjour: " + fn +" || "
+                "Numéro de compte "+account_no +' || '
+                'Félécitations! Vous pouvez prétendre à un prêt bancaire !'
             )
 
 run()
